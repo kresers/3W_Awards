39b3220a861330ae9586decc67ff8bfb346e0b7c
kresers <valentinregnier@free.fr>
2018-01-26 12:12:07 +0100

    Generate back bundle to team work

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new BackBundle\BackBundle(),
+            new FrontBundle\FrontBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/BackBundle/BackBundle.php
+++ src/BackBundle/BackBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace BackBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class BackBundle extends Bundle
 {
 }
