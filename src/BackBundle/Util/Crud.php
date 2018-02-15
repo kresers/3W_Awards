dfd12a2b4e393eca32c270216edca2cca7fd17ae
kresers <valentinregnier@free.fr>
2018-02-15 15:58:36 +0100

    updata service crud

--- src/BackBundle/Util/Crud.php
+++ src/BackBundle/Util/Crud.php
@@ -9,8 +9,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Crud
 {
-    private $container;
-
     private $container; // with this var you can call all service
 
     public function __construct(ContainerInterface $container)
@@ -18,11 +16,9 @@ class Crud
         $this->container = $container;
     }
 
-
     /* this function create and submit entity wich you have get in parameters*/
     public function AddOrEditEntity(Entity $entity)
     {
         $em = $this->container->get('doctrine.orm.entity_manager');
-        $form = 
     }
 }
