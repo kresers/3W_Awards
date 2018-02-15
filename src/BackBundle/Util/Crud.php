7167e9ef168033fcb1518f6acde13cc74ad00915
kresers <valentinregnier@free.fr>
2018-02-15 13:42:59 +0100

    service crd update

--- src/BackBundle/Util/Crud.php
+++ src/BackBundle/Util/Crud.php
@@ -3,14 +3,26 @@
 
 namespace BackBundle\Util;
 use Doctrine\ORM\Mapping\Entity;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 
 class Crud
 {
 
+    private $container; // with this var you can call all service
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
 
     /* this function create and submit entity wich you have get in parameters*/
     public function AddOrEditEntity(Entity $entity)
     {
+        $em = $this->container->get('doctrine.orm.entity_manager'); /* @var \Doctrine\ORM\EntityManager $em */
+        $entityName = get_class($entity);
+        dump($entityName);die;
+//        $entityExist = $em->getRepository('AppBundle')
     }
 }
