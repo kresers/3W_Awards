ddacc9430d2689c8fdf118e9ebc4ed567bd3b1bd
kresers <valentinregnier@free.fr>
2018-02-15 14:57:07 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/BackBundle/Util/Crud.php
+++ src/BackBundle/Util/Crud.php
@@ -2,15 +2,24 @@
 
 
 namespace BackBundle\Util;
+
 use Doctrine\ORM\Mapping\Entity;
+use Psr\Container\ContainerInterface;
 
 
 class Crud
 {
+    private $container;
 
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
 
     /* this function create and submit entity wich you have get in parameters*/
     public function AddOrEditEntity(Entity $entity)
     {
+        $em = $this->container->get('doctrine.orm.entity_manager');
+        $form = 
     }
 }
