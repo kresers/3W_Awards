d814ad7cb8020115395bd8749730ed0ee9ad7e77
kresers <valentinregnier@free.fr>
2018-02-15 11:49:33 +0100

    Correction listener postPersist

---
+++ src/BackBundle/Util/Crud.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace BackBundle\Util;
+use Doctrine\ORM\Mapping\Entity;
+
+
+class Crud
+{
+
+
+    /* this function create and submit entity wich you have get in parameters*/
+    public function AddOrEditEntity(Entity $entity)
+    {
+    }
+}
