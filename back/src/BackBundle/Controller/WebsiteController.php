cb91734b4772f8e6ea3a27319ccb3a18155a17ce
MaximeBordas <maxime.bordas01@gmail.com>
2018-04-15 13:04:54 +0200

    api Website start

---
+++ back/src/BackBundle/Controller/WebsiteController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace BackBundle\Controller;
+
+use BackBundle\Entity\Project;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WebsiteController extends Controller
+{
+    /**
+     * @return array
+     * @Rest\View()
+     */
+    public function getWebsiteAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $allwebsite = $em->getRepository(Project::class)->findAll();
+
+        return array('website' => $allwebsite);
+    }
+
+}
