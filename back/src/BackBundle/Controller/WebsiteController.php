ac2a84b5017a3950f08b22048f2837ed7912c6be
MaximeBordas <maxime.bordas01@gmail.com>
2018-04-17 15:01:07 +0200

    refactor controller

--- back/src/BackBundle/Controller/WebsiteController.php
+++ back/src/BackBundle/Controller/WebsiteController.php
@@ -3,23 +3,24 @@
 namespace BackBundle\Controller;
 
 use BackBundle\Entity\Project;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
 
-class WebsiteController extends Controller
+class WebsiteController
 {
     /**
-     * @return array
-     * @Rest\View()
+     * @Get(
+     *     path = "/website/{id}",
+     *     name = "app_website_show",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @View
      */
-    public function getWebsitesAction()
+    public function showAction()
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $allwebsite = $em->getRepository(Project::class)->findAll();
-
-        return array('website' => $allwebsite);
+        $website = new  Project();
+        $website->setProjectName('Test');
+        return $website;
     }
 
 }
