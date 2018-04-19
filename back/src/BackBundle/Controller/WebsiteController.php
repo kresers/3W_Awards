f52a19784c8e9060192e6f29130e639e634ffdde
MaximeBordas <maxime.bordas01@gmail.com>
2018-04-19 10:08:05 +0200

    get Api working for All agencies and one given agency

--- back/src/BackBundle/Controller/WebsiteController.php
+++ back/src/BackBundle/Controller/WebsiteController.php
@@ -3,10 +3,12 @@
 namespace BackBundle\Controller;
 
 use BackBundle\Entity\Project;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\View;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class WebsiteController
+class WebsiteController extends Controller
 {
     /**
      * @Get(
@@ -15,12 +17,29 @@ class WebsiteController
      *     requirements = {"id"="\d+"}
      * )
      * @View
+     *
      */
     public function showAction()
     {
-        $website = new  Project();
-        $website->setProjectName('Test');
+        $website = new Project();
+        $website->setDevLanguages('Symfony');
         return $website;
     }
+
+    /**
+     * @Get(
+     *     path="/websites",
+     *     name="app_websites_all_show"
+     * )
+     *
+     * @View
+     */
+    public function showAllAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $allWebsites = $em->getRepository(Project::class)->findAll();
+        return $allWebsites;
+    }
+
 
 }
