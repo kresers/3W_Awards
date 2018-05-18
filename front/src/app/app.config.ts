ad0f236ee623d0073f84fdf68451ee8612b3f179
MaximeBordas <maxime.bordas01@gmail.com>
2018-05-18 10:45:05 +0200

    add link for pictures

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -2,4 +2,7 @@ export const appConfig = {
   apiUrl: 'http://localhost:4200/administration/api',
   loginUrl: 'http://localhost:4200/api/login_check',
   backLoginUrl: 'http://localhost:4200/admin',
+  customerPictureUrl: 'http://localhost/3w_awards/back/web/uploads/images/customers/',
+  agencyPictureUrl: 'http://localhost/3w_awards/back/web/uploads/images/agency/',
+  websitePictureUrl: 'http://localhost/3w_awards/back/web/uploads/images/website/',
 };
