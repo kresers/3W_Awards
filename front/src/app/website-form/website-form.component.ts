8b37c271dcebf3f17a7084eddbb9c3928a27ab92
Unknown <thomas.focqueu@hotmail.fr>
2018-05-31 09:00:19 +0200

    fix bug master

--- front/src/app/website-form/website-form.component.ts
+++ front/src/app/website-form/website-form.component.ts
@@ -128,7 +128,7 @@ export class WebsiteFormComponent implements OnInit {
       technology: technologyDeepCopy,
       award: awardDeepCopy,
       country: coutryDeepCopy,
-      isNominated: formModel.isNominated
+      is_nominated: formModel.is_nominated as boolean
     };
     return saveWebsite;
   }
